dc0b6b69c9a2ed35e905b02dccd0dbacb43d7cf8
minju <52149296+minzoovv@users.noreply.github.com>
2019-12-31 03:14:43 +0900

    <init> change link

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const Footer = () => (
   <footer className="footer">
     ©<a href="https://github.com/minzoovv">minzoovv</a>, Built with{' '}
-    <a href="https://github.com/minzoovv/minzoovv.dev">
+    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
   </footer>
